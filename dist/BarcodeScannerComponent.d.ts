ae7fb4a8e87981d4015ac1320c4586592c8ccad8
Rose Pineau <rosepineau@protonmail.com>
2023-04-13 09:26:33 -0400

    Npm install.

--- dist/BarcodeScannerComponent.d.ts
+++ dist/BarcodeScannerComponent.d.ts
@@ -1,11 +1,11 @@
-import React from "react";
-import { Result } from "@zxing/library";
-declare const BarcodeScannerComponent: ({ onUpdate, onError, width, height, facingMode, torch, delay, videoConstraints, stopStream, }: {
+import React from 'react';
+import { Result } from '@zxing/library';
+declare const BarcodeScannerComponent: ({ onUpdate, onError, width, height, facingMode, torch, delay, videoConstraints, stopStream }: {
     onUpdate: (arg0: unknown, arg1?: Result) => void;
     onError?: (arg0: string | DOMException) => void;
     width?: number | string;
     height?: number | string;
-    facingMode?: "environment" | "user";
+    facingMode?: 'environment' | 'user';
     torch?: boolean;
     delay?: number;
     videoConstraints?: MediaTrackConstraints;
--- dist/BarcodeScannerComponent.js
+++ dist/BarcodeScannerComponent.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const react_1 = __importDefault(require("react"));
 const library_1 = require("@zxing/library");
 const react_webcam_1 = __importDefault(require("react-webcam"));
-const BarcodeScannerComponent = ({ onUpdate, onError, width = "100%", height = "100%", facingMode = "environment", torch, delay = 500, videoConstraints, stopStream, }) => {
+const BarcodeScannerComponent = ({ onUpdate, onError, width = '100%', height = '100%', facingMode = 'environment', torch, delay = 500, videoConstraints, stopStream }) => {
     const webcamRef = react_1.default.useRef(null);
     const capture = react_1.default.useCallback(() => {
         var _a;
@@ -26,23 +26,18 @@ const BarcodeScannerComponent = ({ onUpdate, onError, width = "100%", height = "
     react_1.default.useEffect(() => {
         var _a, _b;
         // Turn on the flashlight if prop is defined and device has the capability
-        if (typeof torch === "boolean" && ((_a = 
+        if (
         // @ts-ignore
-        navigator === null || 
-        // @ts-ignore
-        navigator === void 0 ? void 0 : 
-        // @ts-ignore
-        navigator.mediaDevices) === null || _a === void 0 ? void 0 : _a.getSupportedConstraints().torch)) {
+        typeof torch === 'boolean' && ((_a = navigator === null || navigator === void 0 ? void 0 : navigator.mediaDevices) === null || _a === void 0 ? void 0 : _a.getSupportedConstraints().torch)) {
             const stream = (_b = webcamRef === null || webcamRef === void 0 ? void 0 : webcamRef.current) === null || _b === void 0 ? void 0 : _b.video.srcObject;
             const track = stream === null || stream === void 0 ? void 0 : stream.getVideoTracks()[0]; // get the active track of the stream
             if (track &&
                 track.getCapabilities().torch &&
                 !track.getConstraints().torch) {
-                track
-                    .applyConstraints({
-                    advanced: [{ torch }],
-                })
-                    .catch((err) => onUpdate(err));
+                // @ts-ignore
+                track.applyConstraints({
+                    advanced: [{ torch }]
+                }).catch((err) => onUpdate(err));
             }
         }
     }, [torch, onUpdate]);
@@ -51,7 +46,8 @@ const BarcodeScannerComponent = ({ onUpdate, onError, width = "100%", height = "
         if (stopStream) {
             let stream = (_a = webcamRef === null || webcamRef === void 0 ? void 0 : webcamRef.current) === null || _a === void 0 ? void 0 : _a.video.srcObject;
             if (stream) {
-                stream.getTracks().forEach((track) => {
+                stream.getTracks()
+                    .forEach((track) => {
                     stream.removeTrack(track);
                     track.stop();
                 });
@@ -66,7 +62,7 @@ const BarcodeScannerComponent = ({ onUpdate, onError, width = "100%", height = "
         };
     }, []);
     return (react_1.default.createElement(react_webcam_1.default, { width: width, height: height, ref: webcamRef, screenshotFormat: "image/jpeg", videoConstraints: videoConstraints || {
-            facingMode,
+            facingMode
         }, audio: false, onUserMediaError: onError }));
 };
 exports.default = BarcodeScannerComponent;
